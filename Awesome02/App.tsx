e7a525d15b2bf865c54cb654788f311f22282819
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-15 20:38:03 +0530

    new commit 🚀

---
+++ Awesome02/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello App</Text>
+        <Text>Hello App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
