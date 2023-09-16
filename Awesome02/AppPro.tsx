a1bd5c809e34812ce6b047eedd297144ec75a8d8
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-16 20:00:21 +0530

    new commit 🚀

--- Awesome02/AppPro.tsx
+++ Awesome02/AppPro.tsx
@@ -1,12 +1,14 @@
 import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
+import ElivatedCards from './components/ElivatedCards';
 
 const AppPro = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCards />
+        <ElivatedCards />
       </ScrollView>
     </SafeAreaView>
   );
