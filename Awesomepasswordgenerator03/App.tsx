fb748d88116abac5e093c2f828712786e3fd6193
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-17 19:20:49 +0530

    new commit 🚀

---
+++ Awesomepasswordgenerator03/App.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={styles.main}>App There</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  main: {
+    color: 'red',
+    fontSize: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
