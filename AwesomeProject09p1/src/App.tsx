c353964bffedabdea5a20f9d09b7cac1d580a17e
gitdeepaks <deepaksankhyan92@outlook.com>
2023-09-29 10:48:26 +0530

    new commit 🚀

---
+++ AwesomeProject09p1/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// Navigation//
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {screensEnabled} from 'react-native-screens';
+
+//screens
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {productId: string};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Trending Products',
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            title: 'Products Details',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ AwesomeProject09p1/src/screens/Details.tsx
@@ -0,0 +1,41 @@
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+// Navigation
+
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+const Details = ({route}: DetailsProps) => {
+  const {productId} = route.params;
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.smallText}>Details: {productId}</Text>
+      <Button
+        title="Go back to first screen"
+        // onPress={() => navigation.navigate('Home')}
+        // onPress={() => navigation.pop()}
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+};
+
+export default Details;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  smallText: {
+    color: 'black',
+  },
+});
