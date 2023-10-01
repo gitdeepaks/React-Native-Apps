525749c9c75e25dd6f7a0c27dfd7c7630610dae7
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-01 19:26:47 +0530

    new commit 🚀

--- AwesomeProject10/src/App.tsx
+++ AwesomeProject10/src/App.tsx
@@ -1,17 +1,28 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {AuthStackParamList} from './types';
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import Home from './screens/Home';
 import SignUp from './screens/SignUp';
 import Login from './screens/Login';
 
 const App = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AuthStackParamList, 'SignUp'>>();
+  const route = useRoute<RouteProp<AuthStackParamList, 'SignUp'>>();
+
   return (
-    <SafeAreaView>
-      <Home />
-      <SignUp />
-      <Login />
+    <SafeAreaView style={styles.container}>
+      <SignUp navigation={navigation} route={route} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
