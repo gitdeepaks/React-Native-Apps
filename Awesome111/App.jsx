d399b68db9032b4e960e905b4cac8c37e1a1a2bb
gitdeepaks <deepaksankhyan92@outlook.com>
2024-02-22 19:43:34 +0530

    new commit 🚀

---
+++ Awesome111/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 20, textAlign: 'center', marginTop: 20}}>
+        Hello World how are you all
+      </Text>
+    </View>
+  );
+};
+
+export default App;
